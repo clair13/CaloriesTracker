72acb4ae955fb4dc4bcb5619a62403b98a9fe03a
clair13 <theclairvoyant1313@yahoo.gr>
2020-04-28 19:16:15 +0300

    Implement controllers and data structure

---
+++ app.js
@@ -0,0 +1,48 @@
+//Item controller
+
+const ItemCtrl = (function(){
+  //Item constractor
+  const Item = function(id,name,calories){
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+
+  //Data Structure /State
+  const data = {
+    items: [ 
+      {id: 0, name: 'Steak Dinner', calories: 1200},
+      {id: 1, name: 'Cookie', calories: 400},
+      {id: 2, name: 'Eggs', calories: 300}
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  // Public methods
+  return {
+    logData: function(){
+      return data;
+    }
+  }
+})();
+
+//UI Controller
+const UICtrl = (function(){
+  return {
+
+  }
+})();
+
+//App Controller
+const App = (function(ItemCtrl, UICtrl){
+  return { 
+    init: function(){
+      console.log('Initialize App');
+    }
+  }
+
+})(ItemCtrl, UICtrl);
+
+//Initialize App
+App.init();
