657209e038d41863009f5b118e6003ace5cbe815
clair13 <theclairvoyant1313@yahoo.gr>
2020-04-29 20:17:48 +0300

    Implement method for total calories in UI

--- app.js
+++ app.js
@@ -43,6 +43,20 @@ const ItemCtrl = (function(){
 
       return newItem;
     },
+    getTotalCalories: function(){
+      let total = 0;
+
+      // Loop through items and add cals
+      data.items.forEach(function(item){
+        total += item.calories;
+      });
+
+      // Set total cal in data structure
+      data.totalCalories = total;
+
+      // Return total
+      return data.totalCalories;
+    },
     logData: function(){
       return data;
     }
@@ -55,7 +69,8 @@ const UICtrl = (function(){
     itemList: '#item-list',
     addBtn: '.add-btn',
     itemNameInput: '#item-name',
-    itemCaloriesInput: '#item-calories'
+    itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories'
   }
 
 
@@ -105,6 +120,9 @@ const UICtrl = (function(){
     hideList: function(){
       document.querySelector(UISelectors.itemList).style.display = 'none';
     },
+    showTotalCalories: function(totalCalories){
+      document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+    },
     getSelectors: function(){
       return UISelectors;
     }
@@ -135,6 +153,11 @@ const App = (function(ItemCtrl, UICtrl){
       //Add item to UI list
       UICtrl.addListItem(newItem);
 
+       // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+
       //clear fields
       UICtrl.clearInput();
     }
@@ -155,6 +178,11 @@ const App = (function(ItemCtrl, UICtrl){
         // Populate list with items
         UICtrl.populateItemList(items);
       }
+
+       // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
 
       // Load Event Listeners
       loadEventListeners();
